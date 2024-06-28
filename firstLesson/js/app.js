5fdb5143bba808d9cfa976afa9ce2aa4e03448f5
omonjonovm <omonjonovmurodjon2@gmail.com>
2024-06-28 16:56:56 +0500

    java scriptga kirish

---
+++ firstLesson/js/app.js
@@ -0,0 +1,16 @@
+let number = 10;
+const color = "blue";
+console.log(number);
+
+number = 20;
+console.log(number);
+
+const person = {
+  name:"Murodjon"
+}
+person.name = "Jasur"
+console.log(person.name);
+
+// eski versiya
+console.log(studentName);
+var studentName = "Murodjon";

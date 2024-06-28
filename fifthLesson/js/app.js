98b2f4f17fb9a0d1f4d253a84437db90bc532217
omonjonovm <omonjonovmurodjon2@gmail.com>
2024-06-28 21:43:52 +0500

    operatorlar

---
+++ fifthLesson/js/app.js
@@ -0,0 +1,23 @@
+"use strict"
+
+// console.log(9 + +"200");
+
+//incr,decr
+// let incr = 10;
+// let decr = 10;
+
+
+// console.log(incr++);
+// console.log(decr--);
+
+// console.log(5 % 2);
+
+// console.log(5*2 == "10");
+
+// &&  = va  || = yoki
+// const isAge = true;
+// const isClose = true;
+// const isChecked = false;
+// console.log(isAge &&  isClose && !isChecked);
+
+console.log(2 + 2 * 2 !== 6);

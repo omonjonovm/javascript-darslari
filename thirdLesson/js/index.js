94ae0ffa2e49b44dae6290bae3b8810c647ae64e
omonjonovm <omonjonovmurodjon2@gmail.com>
2024-06-28 20:20:58 +0500

    foydalanuvchi bilan muloqot

---
+++ thirdLesson/js/index.js
@@ -0,0 +1,19 @@
+"use strict"
+// alert("Hello js")
+
+// const place = confirm("are you ok?")
+// console.log(place);
+// biz qachonki confirm ishlatganimizda ikkita qiymat qaytaradi : true/false
+
+// const currentPlace =prompt("where are you?", "Home")
+// console.log(currentPlace); 
+
+// const age = +prompt("what how old are you?","16");
+// console.log(age + 10); // "18" + 10 = 28
+
+const favouriteColor = [];
+favouriteColor[0]  = prompt("What's your favourite  color #1" , "");
+favouriteColor[1]  = prompt("What's your favourite  color #2" , "");
+favouriteColor[2]  = prompt("What's your favourite  color #3" , "");
+
+console.log(typeof favouriteColor);

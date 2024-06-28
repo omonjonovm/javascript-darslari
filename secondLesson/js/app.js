32354be399cfde461e8fdde54d3e350aa440afd1
omonjonovm <omonjonovmurodjon2@gmail.com>
2024-06-28 19:04:28 +0500

    primitive data types

--- secondLesson/js/app.js
+++ secondLesson/js/app.js
@@ -1,4 +1,23 @@
 "use strict";
 
-const number = 10;
-console.log(number);
+//Number
+let number = 5.6; //Data Type = Number
+console.log(-8 / 0); //Infinity
+console.log("Murodjon" * 8); //NaN
+
+//String
+const studentName = "Murodjon";
+
+//Boolean
+const isMarried = false;
+const isCircleEarth = true;
+
+// true = xa | to'g'ri
+// false = yo'q | not'g'ri
+
+// null
+console.log(clientName);
+
+// undefind
+let dvc;
+console.log(dvc);

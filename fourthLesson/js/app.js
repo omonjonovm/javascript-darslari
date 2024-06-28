d1a95ed20d5df52b7875202f8d1b364d671a88d6
omonjonovm <omonjonovmurodjon2@gmail.com>
2024-06-28 20:54:21 +0500

    interpolatsiya

---
+++ fourthLesson/js/app.js
@@ -0,0 +1,9 @@
+"use strict"
+
+// const channel = prompt("What's your favourite you tuber")
+// console.log(`https://youtube.com/${channel}`);
+
+const user = "Murodjon";
+const job = "Student";
+
+console.log(`User name is ${user}, he is ${job}`);

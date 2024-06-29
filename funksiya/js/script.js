5897d2014ce518fffd460c7ae1a81fe63519285f
omonjonovm <omonjonovmurodjon2@gmail.com>
2024-06-29 16:45:48 +0500

    function

---
+++ funksiya/js/script.js
@@ -0,0 +1,35 @@
+"use setrict"
+
+// Function Declaration 
+// function calc(a, b) {
+//   console.log(a + b);
+// }
+// calc(10,5)
+// calc(9,10)
+
+// function sayMyName(name,surame) {
+//    age =  16;
+//   console.log(`My name is ${name} ${surame}`);
+//   console.log(age);
+//   return
+// }
+// sayMyName("Murodjon","Omonjonov");
+// console.log(age);
+
+// const sumOfAandB = calc(10 ,5)
+// console.log(sumOfAandB);
+
+// function calc(a, b) {
+//   return a + b;
+// }
+
+// Function Expression
+// const logger =  function() {
+//   console.log("dd");
+// };
+// logger();
+
+
+// Arrow Function
+const calc = (a,b) =>  a + b;
+console.log(calc(47,9)); 

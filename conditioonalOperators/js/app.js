a1a5568f5a521f436fdfb432691c97c798fee29f
omonjonovm <omonjonovmurodjon2@gmail.com>
2024-06-29 10:24:04 +0500

    if,else,else if

---
+++ conditioonalOperators/js/app.js
@@ -0,0 +1,36 @@
+"use strict"
+
+// const age = +prompt("How old are you", "");
+
+// if(age > 18) {
+//   console.log("welcome");
+// } else {
+//   console.log("no entery");
+// } 
+
+// if(age > 25) {
+//   console.log("Horror films");
+// } else if(age > 18 && age < 25) {
+//   console.log("Boyevik");
+// } else {
+//   console.log("Animation");
+// }
+
+// (age > 25 ) ? console.log("Horror films") :console.log("Animation");
+
+const color = "crash";
+
+switch (color) {
+  case "red":
+    console.log("Stop");
+    break;
+  case "green":
+    console.log("Go");
+    break;
+  case "yellow":
+    console.log("get ready");
+    break;
+  default:
+    console.log("Traffic jam");
+    break;
+}

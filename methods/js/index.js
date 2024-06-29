65d0833f25740b145449c03b5c84791533cc1e28
omonjonovm <omonjonovmurodjon2@gmail.com>
2024-06-29 17:28:32 +0500

    methods

---
+++ methods/js/index.js
@@ -0,0 +1,25 @@
+"use strict"
+
+// const user = "Devis";
+// const arr = [47,9,17,10,21,4,9];
+
+// console.log(arr[2]); 
+
+// console.log(user.toUpperCase());
+// console.log(user.toLocaleLowerCase());
+
+// console.log(user);
+
+// const user = "brahim dias";
+
+// console.log(user.indexOf("q"));
+// console.log(user.slice(7,12));
+// console.log(user.substring(0,6));
+// console.log(user.substr(7,4));
+
+// const number = 9.7;
+// console.log(Math.round(number));
+
+const borderWidth = "17.49px";
+// console.log(parseInt(borderWidth));
+console.log(parseFloat(borderWidth));

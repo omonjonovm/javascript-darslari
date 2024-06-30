4f094974af340719cd0d658b82a836749fd5432a
omonjonovm <omonjonovmurodjon2@gmail.com>
2024-06-30 16:20:28 +0500

    spread 2

---
+++ spread2/js/index.js
@@ -0,0 +1,22 @@
+"use strict"
+
+// const calc =[17,47,19];
+
+// function logger(x, y, z) {
+//   console.log(x + y + z);
+// }
+
+// logger(...calc);
+
+// const arr = ['x','y']
+
+// const newArr = [...arr]
+
+const  number = {
+  x:10,
+  y:5,
+}
+
+const newNumber = {...number  }
+console.log(number);
+console.log(newNumber);

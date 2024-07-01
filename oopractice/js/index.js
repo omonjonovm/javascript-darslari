530ca42828ac6fb5a1e62a0cab05b9d82199ed9d
omonjonovm <omonjonovmurodjon2@gmail.com>
2024-07-01 13:25:19 +0500

    oop-2

---
+++ oopractice/js/index.js
@@ -0,0 +1,40 @@
+"use strict"
+
+// class Person {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+
+//   greet() {
+//     console.log(`Hello my name is ${this.name} and I ${this.age} years old`);
+//   }
+// }
+// let person = new Person("Devis", 17)
+// person.greet()
+
+// function addEmployee(name,age) {
+//   return{
+//     name:name,
+//     age:age,
+//     greet() {
+//       console.log(`Hello my name is ${this.name} and I ${this.age} years old`);
+//     }
+//   };
+// }
+// const employee = addEmployee("Devis",17);
+// employee.greet()
+
+class Car {
+  constructor(color, model, speed) {
+    this.color = color;
+    this.model = model;
+    this.speed = speed;
+  }
+  drive() {
+    console.log("The Car is drivng");
+  }
+}
+
+let myCar = new Car("red","Toyota",200);
+console.log(myCar);

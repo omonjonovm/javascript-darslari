a0849f49b14e0e8ca4b17c5fde9700eab1594b34
omonjonovm <omonjonovmurodjon2@gmail.com>
2024-07-03 21:46:26 +0500

    dynamic typing

---
+++ dynamicTyping/js/app.js
@@ -0,0 +1,35 @@
+"use strict"
+
+// string
+// console.log(typeof String(17));
+
+
+// console.log( typeof ('kdb '+  '' + 17));
+// const youtubeChannel = 'mancity'
+// console.log('youtube.com/channel/' + youtubeChannel);
+
+
+// number 
+
+// console.log(typeof Number('47'));
+
+// console.log(typeof + 19);
+
+// console.log(typeof (parseInt('17')));
+
+//boolean : 0 , "", null , undefind , NaN = false every time
+
+let  age = 0
+
+if(age) {
+  console.log('kdb');
+}
+age = 32
+if(age) {
+  console.log('kdb');
+}
+
+console.log(typeof Boolean('17'));
+
+
+console.log(typeof (!!"17"));

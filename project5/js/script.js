e4c36bf48b0557eae9f1665b07251e8a45d9b08a
omonjonovm <omonjonovmurodjon2@gmail.com>
2024-07-06 15:05:14 +0500

    practice 5 updated

--- project5/js/script.js
+++ project5/js/script.js
@@ -1,34 +1,85 @@
 "use strict"
-const adv = document.querySelectorAll('.promo__adv img'),
-  wrapper = document.querySelector('.promo__bg'),
-  genre = wrapper.querySelector('.promo__genre'),
-  seriesList = document.querySelector('.promo__interactive-list')
 
-const seriesDB = {
-  series: [
-    'Omar',
-    'Ertugrul',
-    'Bozkir Arslani',
-    'Mgnifisent Century',
-    'The Great Saljuks Guardians',
-  ],
-}
+document.addEventListener("DOMContentLoaded", () => {
+  const adv = document.querySelectorAll('.promo__adv img'),
+    wrapper = document.querySelector('.promo__bg'),
+    genre = wrapper.querySelector('.promo__genre'),
+    seriesList = document.querySelector('.promo__interactive-list'),
+    addForm = document.querySelector('form.add'),
+    inputVal = addForm.querySelector('.adding__input'),
+    checkbox = addForm.querySelector("[type='checkbox']")
 
-adv.forEach((item =>
-  item.remove()
-))
+  const seriesDB = {
+    series: [
+      'OMAR',
+      'The Final Legacy',
+      'ERTUGRUL',
+      'Mgnifisent Century',
+      'The Great Saljuks Guardians',
+    ],
+  }
 
-genre.textContent = 'History'
+  addForm.addEventListener('submit', (event) => {
+    event.preventDefault()
 
-wrapper.style.backgroundImage = 'url("img/91.jpg")'
+    let newSeries = inputVal.value
+    const favo = checkbox.checked
 
-seriesList.innerHTML = ''
+    if (newSeries) {
+      if (newSeries.length > 18) {
+        newSeries = `${newSeries.substring(0, 18)}...`
+      }
 
-seriesDB.series.forEach((item, index) => {
-  seriesList.innerHTML += `
-    <li class="promo__interactive-item">
-    ${index +1}  ${item}
-     <div class="delete"></div>
-    </li>
-   `
+      if (favo) {
+        console.log("Sevimli serial qo'shildi ✅ ");
+      }
+      seriesDB.series.push(newSeries)
+      sortArr(seriesDB.series)
+
+      createSeriesList(seriesDB.series, seriesList)
+    }
+    event.target.reset()
+  })
+
+  const deleteAdv = (arr) => {
+    adv.forEach((item =>
+      item.remove()
+    ))
+  }
+
+  const changes = () => {
+    genre.textContent = 'History'
+
+    wrapper.style.backgroundImage = 'url("img/91.jpg")'
+  }
+
+  const sortArr = (arr) => {
+    arr.sort()
+  }
+
+  const createSeriesList = (series, parent) => {
+    parent.innerHTML = ''
+    sortArr(series)
+
+    series.forEach((item, index) => {
+      parent.innerHTML += `
+      <li class="promo__interactive-item">
+      ${index + 1}  ${item}
+       <div class="delete"></div>
+      </li>
+     `
+    })
+    document.querySelectorAll('.delete').forEach((trash, index) => {
+      trash.addEventListener('click', () => {
+        trash.parentElement.remove()
+        seriesDB.series.splice(index, 1)
+        createSeriesList(series, parent)
+
+      })
+    })
+  }
+  sortArr(seriesDB.series)
+  deleteAdv(adv)
+  changes()
+  createSeriesList(seriesDB.series, seriesList)
 })

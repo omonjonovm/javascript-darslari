a0a6e698722c8654f6b6af8da5a118cc12895692
omonjonovm <omonjonovmurodjon2@gmail.com>
2024-07-15 21:33:14 +0500

    classlist

---
+++ 2-bo'lim/classlist/js/app.js
@@ -0,0 +1,25 @@
+"use strict"
+
+const btns = document.querySelectorAll('button')
+
+// console.log(btns[0].classList.length);
+// console.log(btns[0].classList.item(0));
+
+// btns[1].classList.add('blue','some')
+// btns[0].classList.remove('blue')
+// btns[0].classList.toggle('blue')
+
+// btns[0].classList.add('red')
+
+// if(btns[0].classList.contains('red')){
+//   console.log('yes');
+// }
+
+// btns[0].addEventListener('click',() => {
+// if(!btns[1].classList.contains('red')){
+//   btns[1].classList.add('red')
+// } else {
+//   btns[1].classList.remove('red')
+// }
+btns[1].classList.toggle('red')
+//` })

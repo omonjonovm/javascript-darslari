24b2df3d222b62efd9f9609a0c0055d7647c6efa
omonjonovm <omonjonovmurodjon2@gmail.com>
2024-07-16 15:28:53 +0500

    added loader

--- 2-bo'lim/loyiha/js/script.js
+++ 2-bo'lim/loyiha/js/script.js
@@ -1,20 +1,30 @@
-"use strict"
 window.addEventListener('DOMContentLoaded', () => {
   const tabsParent = document.querySelector('.tabheader__items'),
     tabs = document.querySelectorAll('.tabheader__item'),
     tabsContent = document.querySelectorAll('.tabcontent')
+    loader = document.querySelector('.loader')
+
+  //loader
+  setTimeout(() => {
+    loader.style.opacity= '0'
+    setTimeout(() => {
+      loader.style.display = 'none'
+    },500)
+  }, 2000)
 
   function hideTabContent() {
     tabsContent.forEach((item) => {
       item.classList.add('hide')
-      item.classList.remove('show','fade')
+      item.classList.remove('show', 'fade')
     })
+
+
     tabs.forEach((item) => {
       item.classList.remove('tabheader__item_active')
     })
   }
   function showTabContent(i = 0) {
-    tabsContent[i].classList.add('show','fade')
+    tabsContent[i].classList.add('show', 'fade')
     tabsContent[i].classList.remove('hide')
     tabs[i].classList.add('tabheader__item_active')
   }
